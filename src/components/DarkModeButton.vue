56aee6155290308773ca06718d587dea10f0707c
Eunyeol <ney9083@gmail.com>
2022-05-12 23:48:59 +0900

    Style: aria 속성 부여

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -11,6 +11,7 @@
       viewBox="0 0 24 24"
       stroke="currentColor"
       stroke-width="2"
+      aria-label="Light Mode Button"
     >
       <path
         stroke-linecap="round"
@@ -26,6 +27,7 @@
       viewBox="0 0 24 24"
       stroke="currentColor"
       stroke-width="2"
+      aria-label="Dark Mode Button"
     >
       <path
         stroke-linecap="round"
@@ -33,6 +35,7 @@
         d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
       />
     </svg>
+    <p class="hidden">Dark&Light Toggle Button</p>
   </button>
 </template>
 
